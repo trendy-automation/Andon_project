0ff00589c9ac76443652e7dbfd492323a0729516
trendy-automation <ilya-kolesnic@yandex.ru>
2017-03-23 23:49:46 +0300

    ROOT

--- WebUI/aria/app/view/Tree.js
+++ WebUI/aria/app/view/Tree.js
@@ -1,6 +1,7 @@
-Ext.define('MyTree', {
+Ext.define('TreeObjectNode', {
                    extend: 'Ext.data.Model',
                    fields: [
+                       //{name: 'expanded', type: 'boolean', defaultValue: true, persist: false },
                        {name: 'OBJECT_NAME',  type: 'string'},
                        {name: 'OBJECT_TYPE_NAME',  type: 'string'},
                        {name: 'OBJECT_TYPE_ID',   type: 'int', convert: null},
@@ -9,44 +10,67 @@ Ext.define('MyTree', {
                    ]
            });
 
-var treeStore = Ext.create('Ext.data.TreeStore', {
-                               //fields: ['OBJECT_NAME','OBJECT_TYPE_NAME','OBJECT_TYPE_ID','PKEY','PID'],
+var treeStore = Ext.create('Ext.data.TreeStore',
+                           {
+                               model:'TreeObjectNode',
                                pageSize :0,
                                proxy: {
                                    type: 'ajax',
-                                   url: 'trees',
+                                   url: '/json/treePanel',
                                    reader: {
-                                                   type: 'json',
-                                                   rootProperty: 'children'
+                                                   type: 'json'
                                    }
-                               },
-                               defaultValue: {root:{}},
-                               autoLoad: false
+                               }
+                               ,autoLoad: false
+                               //,autoSync: true
+//                               ,listeners: {
+//                                              load: function (treeStore , records , successful , operation , node , eOpts ) {
+//                                                  //Ext.getCmp('treePanel').expandPath('/root/');
+//                                                   //treePanel.expandPath('/root/');
+//                                                  //console.log('treeStore load',records);
+//                                              }
+//                                          }
+                               //,autoLoad: true,
+                               //,autoSync: true
+                               //clearOnLoad: true,
                  });
 
 Ext.define('AndonPortal.view.Tree', {
     extend: 'Ext.tree.Panel',
     alias:  'widget.mysimpletree',
+    id: 'treePanel',
 //    viewConfig: {plugins: {ptype: 'treeviewdragdrop'}},
     renderTo: Ext.getBody(),
     title: 'TreeGrid',
-//    rootVisible: true,
+    rootVisible: false,
     hideCollapseTool: false,
     collapsible: true,
     width: 300,
     height: 150,
     fields: ['name', 'description'],
     store: treeStore,
+    //autoLoad : true,
     columns: [{
         xtype: 'treecolumn',
         text: 'Andon data',
-        dataIndex: "text",//'OBJECT_NAME',
-        width: 150,
+        dataIndex: 'OBJECT_NAME',
+        width: 300,
         sortable: true
-    }/*, {
+    }/* ,
+    {
         text: 'Description',
         dataIndex: 'description',
         flex: 1,
         sortable: true
     }*/]
+
+
+    ,listeners: {
+               load: function (treeStore , records , successful , operation , node , eOpts ) {
+                    node.expand();
+               }
+           }/*,
+    selection : Ext.create(
+                    'Ext.selection.Model',{
+                    mode:'MULTI'})*/
 });
